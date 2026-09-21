895ab02196e81b57a41669eb21ce2b815d75d774
Ushasree R <ushasree19832000@gmail.com>
2026-09-21 19:35:18 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ duplicate line
 name = input("Enter your name: ")
 message = greet(name)
 hello
+add duplicate line
 print(message)
 print("This is my first Git commit demonstration.")
 print(message)
